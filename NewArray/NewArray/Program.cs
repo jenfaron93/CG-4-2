7627f5eed9a57808ce7332bcc6bad147d9f92e86
Jennifer Faron <jf891w@gmail.com>
2018-07-29 18:40:44 -0500

    declared and intialized an array displayed the array's contents

---
+++ NewArray/NewArray/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NewArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int[] num = new int[] { 1, 1, 2, 3, 5, 8 }; //declares and intializes an int array called 'num'
+			foreach (int x in num){ //runs a loop for each time their is an element (x) in the array
+				Console.WriteLine(x); //writes the element (x) out to the console
+			}
+			Console.ReadLine();
+        }
+    }
+}
