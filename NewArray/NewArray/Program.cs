f3caf70cf0338f60238cab824413042d82a1cd92
Jennifer Faron <jf891w@gmail.com>
2018-07-29 18:42:28 -0500

    update comments

--- NewArray/NewArray/Program.cs
+++ NewArray/NewArray/Program.cs
@@ -7,7 +7,7 @@ namespace NewArray
         static void Main(string[] args)
         {
 			int[] num = new int[] { 1, 1, 2, 3, 5, 8 }; //declares and intializes an int array called 'num'
-			foreach (int x in num){ //runs a loop for each time their is an element (x) in the array
+			foreach (int x in num){ //runs a loop for each time there is an element ( which I named x) in the array
 				Console.WriteLine(x); //writes the element (x) out to the console
 			}
 			Console.ReadLine();
